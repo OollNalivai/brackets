9822cb2399cbf54dc170c52f2c594abc67dc78fd
VladislavOoll <apahiphip@mail.ru>
2021-03-15 20:36:43 +0300

    feat: add code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    str = str.split('');
+    for (let i = 0; i < str.length; i++) {
+        for (let n = 0; n < bracketsConfig.length; n++) {
+            if (str[i] === bracketsConfig[n][0] && str[i + 1] === bracketsConfig[n][1]) {
+                str.splice(i, 2);
+                i = -1;
+            }
+        }
+    }
+    return str.length === 0? true : false;
+};
